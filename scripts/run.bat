cbcc35584240b1db7357c02af3c635dd8e9c4f57
marouane <20244973@etud.univ-evry.fr>
2025-10-07 09:57:58 +0200

    correct run.bat

--- scripts/run.bat
+++ scripts/run.bat
@@ -2,11 +2,17 @@
 echo === Lancement du MiniSGBDR ===
 
 REM Vérifier que les classes sont compilées
-if not exist "build\MiniSGBDR.class" (
+if not exist "..\build\MiniSGBDR.class" (
     echo Les classes ne sont pas compilées. Exécutez d'abord scripts\compile.bat
     exit /b 1
 )
 
 REM Lancer l'application principale
 echo Lancement de l'application...
+REM Change working directory to project root so relative paths (e.g. ./DB) resolve from project root
+pushd .. >nul
 java -cp build MiniSGBDR %*
+set RC=%ERRORLEVEL%
+popd >nul
+
+exit /b %RC%
